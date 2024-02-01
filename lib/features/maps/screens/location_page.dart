1331ff646b14f1b48db19840cfd55ecb41d1f67c
Aditya-1708 <umeshaditya72@gmail.com>
2024-02-01 19:21:19 +0530

    maps added From Abhishek's REPO

--- lib/features/maps/screens/location_page.dart
+++ lib/features/maps/screens/location_page.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:bloodbond_app/features/community/screens/community_page.dart';
 import 'package:bloodbond_app/features/home/screens/home_page.dart';
 import 'package:bloodbond_app/features/profile/screens/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
@@ -11,7 +15,30 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
+  Location _locationController = new Location();
+
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+  static const LatLng _pApplePark = LatLng(37.3346, -122.0090);
+
+  Map<PolylineId, Polyline> polylines = {};
+  LatLng? _currentP = null;
+
   int currentPage = 1;
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+    // .then(
+    //   (_) => {
+    //     getPolylinePoints().then((coordinates) => {
+    //           print(coordinates),
+    //         }),
+    //   },
+    // );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +55,31 @@ class _LocationPageState extends State<LocationPage> {
         ),
       ),
       backgroundColor: Color.fromARGB(255, 255, 255, 236),
+      body: _currentP == null
+          ? Center(
+              child: Text("Loading..."),
+            )
+          : GoogleMap(
+              onMapCreated: ((GoogleMapController controller) =>
+                  _mapController.complete(controller)),
+              initialCameraPosition:
+                  CameraPosition(target: _pGooglePlex, zoom: 13),
+              markers: {
+                Marker(
+                    markerId: MarkerId("_currentLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _currentP!),
+                Marker(
+                    markerId: MarkerId("_sourceLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _pGooglePlex),
+                Marker(
+                    markerId: MarkerId("_destinationLocation"),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _pApplePark),
+              },
+              polylines: Set<Polyline>.of(polylines.values),
+            ),
       bottomNavigationBar: NavigationBar(
         backgroundColor: Color.fromARGB(255, 198, 168, 105),
         destinations: const [
@@ -69,5 +121,42 @@ class _LocationPageState extends State<LocationPage> {
         selectedIndex: currentPage,
       ),
     );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPosition = CameraPosition(target: pos, zoom: 13);
+    await controller
+        .animateCamera(CameraUpdate.newCameraPosition(_newCameraPosition));
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _cameraToPosition(_currentP!);
+        });
+      }
+    });
   }
 }
